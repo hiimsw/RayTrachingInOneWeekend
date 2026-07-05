5e9f716aa14caa38076791aa0dc5afad63e32e1c
hiimsw <entryswlee@gmail.com>
2026-07-05 22:12:07 +0900

    pch.h 개선 및 정리.

--- RayTrachingInOneWeekend/Source/pch.h
+++ RayTrachingInOneWeekend/Source/pch.h
@@ -10,12 +10,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-#if defined(_DEBUG)
-#include <source_location>
-#endif
-
-constexpr char PROJECT_PATH[] = "C:\\Users\\ilias\\Desktop\\Project\\KoruECS\\KoruECS";
-
 #define RELEASE(x) \
 if ((x) != nullptr) \
 { \
@@ -32,18 +26,10 @@ if ((x) != nullptr) \
 } \
 void(0)
 
-#define RELEASE_D2D1(x) \
-if ((x) != nullptr) \
-{ \
-	(x)->Release(); \
-	(x) = nullptr; \
-} \
-void(0)
-
 #define LOG(format, ...) \
 { \
 	char log[256]{}; \
-	sprintf_s(log, sizeof(log), "%s(%d): " format "\n", __FILE__ + sizeof(PROJECT_PATH), __LINE__, __VA_ARGS__); \
+	sprintf_s(log, sizeof(log), "%s(%d): " format "\n", strstr(__FILE__, "Source"), __LINE__, __VA_ARGS__); \
 	printf(log); \
 	OutputDebugStringA(log); \
 } \
@@ -52,7 +38,7 @@ void(0)
 #define LOG_W(format, ...) \
 { \
 	wchar_t log[256]{}; \
-	swprintf_s(log, sizeof(log), "%s(%d): " format "\n", __FILE__ + sizeof(PROJECT_PATH), __LINE__, __VA_ARGS__); \
+	swprintf_s(log, sizeof(log), "%s(%d): " format "\n", strstr(__FILE__, "Source"), __LINE__, __VA_ARGS__); \
 	wprintf(log); \
 	OutputDebugStringW(log); \
 } \
@@ -80,6 +66,14 @@ if (not (expr)) \
 } \
 void(0)
 
+#define VERIFY(expr, format, ...) \
+if (not (expr)) \
+{ \
+	DEBUG_LOG("verify(" #expr ", " format ")", __VA_ARGS__); \
+	DEBUG_BREAK(); \
+} \
+void(0)
+
 #define WARN(expr) \
 if (not (expr)) \
 { \
@@ -94,24 +88,6 @@ if (not (expr)) \
 } \
 void(0)
 
-#if !defined(HR)
-#define HR(x) \
-	{ \
-		HRESULT hr = (x); \
-		if (FAILED(hr)) \
-		{ \
-			LPVOID errorLog = nullptr; \
-			FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER, \
-				nullptr, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), \
-				reinterpret_cast<LPSTR>(&errorLog), 0, nullptr); \
-			LOG("%s", static_cast<char*>(errorLog)); \
-			LocalFree(errorLog); \
-			DEBUG_BREAK(); \
-		} \
-	} \
-	((void)0)
-#endif
-
 #else
 
 #define DEBUG_LOG(...) ((void)0)
@@ -119,13 +95,10 @@ void(0)
 #define DEBUG_BREAK() ((void)0)
 
 #define ASSERT(expr) ((void)0)
-#define MASSERT(expr, format, ...) ((void)0)
+#define MASSERT(expr, msg, ...) ((void)0)
+#define VERIFY(expr, msg, ...) { (expr); } void(0)
 
 #define WARN(expr) ((void)0)
-#define MWARN(expr, format, ...) ((void)0)
-
-#if !defined(HR)
-#define HR(x) (x)
-#endif
+#define MWARN(expr, msg) ((void)0)
 
 #endif
